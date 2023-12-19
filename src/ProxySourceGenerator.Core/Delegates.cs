ae76160268eb7bdf0ecc0f2f00a939a4212fd758
Fatih Tolga Ata <fatih.ata@gordionteknoloji.com>
2023-12-19 11:26:18 +0300

    deterministic nuget package.

--- src/ProxySourceGenerator.Core/Delegates.cs
+++ src/ProxySourceGenerator.Core/Delegates.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ProxySourceGenerator;
 
+/// <summary>
+/// Delegate of intercepting the getter of a property
+/// </summary>
+/// <param name="propertyName"></param>
+/// <param name="getter"></param>
+/// <returns></returns>
 public delegate object InterceptPropertyGetterHandler(string propertyName, Func<object> getter);
+/// <summary>
+/// Delegate of intercepting the setter of a property
+/// </summary>
+/// <param name="propertyName"></param>
+/// <param name="setter"></param>
+/// <param name="value"></param>
 public delegate void InterceptPropertySetterHandler(string propertyName, Action<object> setter, object value);
 
+/// <summary>
+/// Delegate for calling a method.
+/// </summary>
+/// <param name="parameters"></param>
+/// <returns></returns>
 public delegate object InterceptMethodCallerHandler(Dictionary<string, object> parameters);
+/// <summary>
+/// Delegate of intercepting a method.
+/// </summary>
+/// <param name="methodName"></param>
+/// <param name="method"></param>
+/// <param name="parameters"></param>
+/// <returns></returns>
 public delegate object InterceptMethodHandler(
     string methodName,
     InterceptMethodCallerHandler method,
